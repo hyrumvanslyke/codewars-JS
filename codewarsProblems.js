fa5982893de1591c44a8af9d9776347435a29d9a
Hyrum Van Slyke <hyrumevanslyke@gmail.com>
2023-11-28 14:43:46 -0500

    Finished day1 code3

--- codewarsProblems.js
+++ codewarsProblems.js
@@ -36,3 +36,38 @@ function solution(str, ending) {
   console.log(solution('abc', 'gbc'))
 
   /////////////////////////////////////////////////////////////////////////////////
+
+//   Usually when you buy something, you're asked whether your credit card number, phone number or answer to your most secret question is still correct. However, since someone could look over your shoulder, you don't want that shown on your screen. Instead, we mask it.
+
+// Your task is to write a function maskify, which changes all but the last four characters into '#'.
+
+// Examples (input --> output):
+// "4556364607935616" --> "############5616"
+//      "64607935616" -->      "#######5616"
+//                "1" -->                "1"
+//                 "" -->                 ""
+
+// "What was the name of your first pet?"
+// "Skippy" --> "##ippy"
+// "Nananananananananananananananana Batman!" --> "####################################man!"
+
+function maskify(str) {
+    // Check if the string length is greater than 4
+    if (str.length > 4) {
+      // Replace all characters except the last four with '#'
+      return '#'.repeat(str.length - 4) + str.slice(-4);
+    } else {
+      // Return the original string if its length is 4 or less
+      return str;
+    }
+  }
+  
+  // Examples:
+  console.log(maskify("4556364607935616")); // Output: "############5616"
+  console.log(maskify("64607935616"));      // Output: "#######5616"
+  console.log(maskify("1"));                // Output: "1"
+  console.log(maskify(""));                 // Output: ""
+  console.log(maskify("Skippy"));           // Output: "##ippy"
+  console.log(maskify("Nananananananananananananananana Batman!"));
+
+  //////////////////////////////////////////////////////////////////////////////////
