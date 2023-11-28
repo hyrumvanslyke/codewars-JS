891294e618c5719b0f67f9656563aaeef8ceb2f7
Hyrum Van Slyke <hyrumevanslyke@gmail.com>
2023-11-28 14:37:26 -0500

    Finish of day1 code2

--- codewarsProblems.js
+++ codewarsProblems.js
@@ -29,3 +29,10 @@ function solution(str) {
 
 // solution('abc', 'bc')  returns true
 // solution('abc', 'd')  returns false
+function solution(str, ending) {
+    return str.endsWith(ending);
+  }
+
+  console.log(solution('abc', 'gbc'))
+
+  /////////////////////////////////////////////////////////////////////////////////
