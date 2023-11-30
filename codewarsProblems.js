c8df7f49a2c80be717007e155d31d9a6e1c71ea4
Hyrum Van Slyke <hyrumevanslyke@gmail.com>
2023-11-29 22:01:05 -0500

    Finished day2 code2

--- codewarsProblems.js
+++ codewarsProblems.js
@@ -90,3 +90,39 @@ function descendingOrder(n) {
   
     return result;
   }
+
+  /////////////////////////////////////////////////////////////////////////////////
+//   Count the number of Duplicates
+// Write a function that will return the count of distinct case-insensitive alphabetic characters and numeric digits that occur more than once in the input string. The input string can be assumed to contain only alphabets (both uppercase and lowercase) and numeric digits.
+
+// Example
+// "abcde" -> 0 # no characters repeats more than once
+// "aabbcde" -> 2 # 'a' and 'b'
+// "aabBcde" -> 2 # 'a' occurs twice and 'b' twice (`b` and `B`)
+// "indivisibility" -> 1 # 'i' occurs six times
+// "Indivisibilities" -> 2 # 'i' occurs seven times and 's' occurs twice
+// "aA11" -> 2 # 'a' and '1'
+// "ABBA" -> 2 # 'A' and 'B' each occur twice
+
+function countDuplicates(input) {
+    // Convert the input string to lowercase for case-insensitive comparison
+    const lowercaseInput = input.toLowerCase();
+    
+    // Create an object to store character occurrences
+    const charCount = {};
+  
+    // Iterate through each character in the input string
+    for (let char of lowercaseInput) {
+      // Ignore non-alphabetic and non-numeric characters
+      if (/[a-z0-9]/.test(char)) {
+        // If the character is not in the object, add it; otherwise, increment its count
+        charCount[char] = (charCount[char] || 0) + 1;
+      }
+    }
+  
+    // Count the characters that occur more than once
+    const duplicatesCount = Object.values(charCount).filter(count => count > 1).length;
+  
+    return duplicatesCount;
+  }
+  //////////////////////////////////////////////////////////////////////////////
