e00950b83f653b4f65b8e47f7b99884157db0f64
Hyrum Van Slyke <hyrumevanslyke@gmail.com>
2023-12-01 14:40:50 -0500

    Start of day4 code2

--- codewarsProblems.js
+++ codewarsProblems.js
@@ -195,3 +195,13 @@ function formatLikes(names) {
       return `${names[0]}, ${names[1]} and ${othersCount} others like this`;
     }
   }
+
+  //////////////////////////////////////////////////////////////////////////////////
+//   Write a function, which takes a non-negative integer (seconds) as input and returns the time in a human-readable format (HH:MM:SS)
+
+// HH = hours, padded to 2 digits, range: 00 - 99
+// MM = minutes, padded to 2 digits, range: 00 - 59
+// SS = seconds, padded to 2 digits, range: 00 - 59
+// The maximum time never exceeds 359999 (99:59:59)
+
+// You can find some examples in the test fixtures.
