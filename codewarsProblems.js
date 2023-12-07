0cef461c8619c7317aa43a4ff0d5b43301851051
Hyrum Van Slyke <hyrumevanslyke@gmail.com>
2023-12-07 11:25:00 -0500

    Start of code2 12/7

--- codewarsProblems.js
+++ codewarsProblems.js
@@ -249,3 +249,13 @@ function twoSum(numbers, target) {
     // No solution found
     return null;
   }
+
+  //////////////////////////////////////////////////////////////////////
+//   Given two arrays of strings a1 and a2 return a sorted array r in lexicographical order of the strings of a1 which are substrings of strings of a2.
+
+// Example 1:
+// a1 = ["arp", "live", "strong"]
+
+// a2 = ["lively", "alive", "harp", "sharp", "armstrong"]
+
+// returns ["arp", "live", "strong"]
