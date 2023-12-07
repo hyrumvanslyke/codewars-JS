1be7e633d4301c8621968901b0d6e92b5044f060
Hyrum Van Slyke <hyrumevanslyke@gmail.com>
2023-12-07 11:27:29 -0500

    Finished code3 12/7

--- codewarsProblems.js
+++ codewarsProblems.js
@@ -259,3 +259,10 @@ function twoSum(numbers, target) {
 // a2 = ["lively", "alive", "harp", "sharp", "armstrong"]
 
 // returns ["arp", "live", "strong"]
+
+function inArray(array1,array2){
+    return array1
+      .filter(a1 => array2.find(a2 => a2.match(a1)))
+      .sort()
+  }
+  ////////////////////////////////////////////////////////////////////////////
