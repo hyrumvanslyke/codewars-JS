3e1681142364973e9fa9fcc10c4f7e7b35fedd4b
Hyrum Van Slyke <hyrumevanslyke@gmail.com>
2024-01-30 16:31:38 -0500

    Finished perentage question with killian

---
+++ killianOtters.js
@@ -0,0 +1,11 @@
+function otterPopulationPercent(maleOtters, femaleOtters){
+    maleOtters = 13
+    femaleOtters = 14
+    totalOtters = maleOtters+ femaleOtters
+    malePecent = maleOtters/ totalOtters
+    femalePecent = femaleOtters/ totalOtters
+return console.log(`Percent number of males: ${malePecent}% 
+Percent number of Females:${femalePecent}%`)
+
+}
+
