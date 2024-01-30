5fd308f7ab61026f98aa9a53b18d642f9dbfdbe4
Hyrum Van Slyke <hyrumevanslyke@gmail.com>
2024-01-30 16:42:29 -0500

    Finished otter example need to round answers tho

--- killianOtters.js
+++ killianOtters.js
@@ -1,11 +1,9 @@
 function otterPopulationPercent(maleOtters, femaleOtters){
-    maleOtters = 13
-    femaleOtters = 14
     totalOtters = maleOtters+ femaleOtters
     malePecent = maleOtters/ totalOtters
     femalePecent = femaleOtters/ totalOtters
-return console.log(`Percent number of males: ${malePecent}% 
-Percent number of Females:${femalePecent}%`)
+return `male percentage is ${malePecent}, while female percent is ${femalePecent}`
 
 }
 
+console.log(otterPopulationPercent(13, 13))
